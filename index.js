1bb147cb0f818addc922f314b39f128873539118
slopez2020276 <slopez-2020276@kinal.edu.gt>
2022-08-01 12:18:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const {CategoriaDefault} = require('./src/controller/categoria.controller');
   mongoose.Promise = global.Promise;                                                               
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Se encuentra conectado a la base de datos.");
-
-    app.listen(3000, function () {
+    const PORT = process.env.PORT || 3000
+    app.listen(PORT, function () {
         console.log("Se esta corriendo en el puerto 3000")
     })
 
